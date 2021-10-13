82f37831bbe168ce566301fd1eb777ce3f28423b
P.F. Lippolis <pf.lippolis@gmail.com>
2021-10-13 17:06:32 +0200

    Add movie model

---
+++ app/src/main/java/com/example/retrofitrecycle/model/Movie.kt
@@ -0,0 +1,6 @@
+package com.example.retrofitrecycle.model
+
+data class Movie(var category: String?,
+                 var imageUrl: String?,
+                 var name: String?,
+                 var desc: String?)
