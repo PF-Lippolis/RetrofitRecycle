56eb828f981737fc516b6b991b75b8a5a397acc6
P.F. Lippolis <pf.lippolis@gmail.com>
2021-10-13 17:15:07 +0200

    Add ApiManager

---
+++ app/src/main/java/com/example/retrofitrecycle/network/ApiManager.kt
@@ -0,0 +1,19 @@
+package com.example.retrofitrecycle.network
+
+import com.example.retrofitrecycle.service.MovieService
+import retrofit2.Retrofit.Builder
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiManager {
+
+    companion object {
+        val BASE_URL = "https://howtodoandroid.com/"
+        val retrofit = Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+
+        val userService = retrofit.create(MovieService::class.java)
+    }
+
+}
--- app/src/main/java/com/example/retrofitrecycle/service/MovieService.kt
+++ app/src/main/java/com/example/retrofitrecycle/service/MovieService.kt
@@ -4,9 +4,9 @@ import com.example.retrofitrecycle.model.Movie
 import retrofit2.http.GET
 import retrofit2.Call
 
-interface UserService {
+interface MovieService {
 
     @GET("movielist.json")
     fun getMovieList(): Call<List<Movie>>
-    
+
 }
